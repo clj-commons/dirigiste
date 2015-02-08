3106c9ef31e15c6b08e0beeee0668bf9c1c1db71
Zach Tellman <ztellman@gmail.com>
2015-02-08 15:21:27 -0800

    back to snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.aleph/dirigiste "0.1.0-alpha7"
+(defproject io.aleph/dirigiste "0.1.0-SNAPSHOT"
   :deploy-repositories {"releases" {:url "https://oss.sonatype.org/service/local/staging/deploy/maven2/"
                                     :creds :gpg}
                         "snapshots" {:url "https://oss.sonatype.org/content/repositories/snapshots/"
