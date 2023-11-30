5ceda3ae5a800abcf0868a74115fab45fefb3fb8
Alexander Yakushev <alex@bytopia.org>
2023-11-30 09:23:39 +0200

    Release 1.0.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clj-commons/dirigiste "1.0.3"
+(defproject org.clj-commons/dirigiste "1.0.4"
   :deploy-repositories [["clojars" {:url "https://repo.clojars.org"
                                     :username :env/clojars_username
                                     :password :env/clojars_password
